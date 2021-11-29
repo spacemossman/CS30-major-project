41077a175bd87f0f3dfe3330a9bfab8690422a4d
spacemossman <91297737+spacemossman@users.noreply.github.com>
2021-11-29 12:11:36 -0600

    added fran's room

--- sketch.js
+++ sketch.js
@@ -6,22 +6,46 @@
 // - describe what you did to take this project "above and beyond"
 
 let invisbleGrid; 
+let gridSize  = 30;
+let levelOneRoom;
+
+function preload(){
+  levelOneRoom = loadImage("assets/franbow'sroom.jpg");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  invisbleGrid = createEmptyArray(gridSize, gridSize);
 }
 
 function draw() {
-
+  displayGrid();
+  image(levelOneRoom, 0, 0, windowWidth, windowHeight);
 }
 
-class Display{
-  constructor(x, y){
-    this.x = x;
-    this.y = y; 
+function displayGrid(){
+  let cellWidth = width/gridSize;
+  let cellHeight = height/gridSize;
+
+  for (let y = 0; y<gridSize; y++){
+    for(let x =0; x<gridSize; x++){
+      rect(x*cellWidth, y * cellHeight, cellWidth, cellHeight);
+    }
   }
 }
 
-function createEmptyArray(){
-   
+function createEmptyArray(rows, cols){
+  let grid = [];
+  
+  for (let y = 0; y<rows; y++){
+    grid.push([]);
+    for(let x = 0; x<cols; x++){
+      grid[y].push(0); 
+    }
+  }
+  return grid;
+}
+
+function mousePressed(){
+  
 }
