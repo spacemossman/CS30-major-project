2fc47f92caf4686c759c8f0bb8ae26b1fb2637cf
spacemossman <91297737+spacemossman@users.noreply.github.com>
2021-12-14 12:11:40 -0600

    Made pill bottle clickable and working on cahnging the background

--- sketch.js
+++ sketch.js
@@ -6,39 +6,57 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let levelOneRoom, pillBottle;
-let picturesF;
+let levelOneRoom, levelTwoRoom, pillBottle;
+let picturesF, theBackground;
 
 function preload(){
   levelOneRoom = loadImage("assets/franbow'sroom.jpg");
   pillBottle = loadImage("assets/nobackgroundDrugs.jpg");
+  // levelTwoRoom = loadImage("assets/")
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   picturesF = new Duotine( 1450, 650, mouseX, mouseY);
+  theBackground = new Background();
 }
 
 function draw() {
-  image(levelOneRoom, 0, 0, windowWidth, windowHeight);
+  theBackground.display();
   picturesF.display();
 }
+class Background{
+  constructor(){
+    this.x = windowHeight;
+    this.y = windowWidth;
+  }
+
+  display(){
+    if(picturesF.clicked === false){
+      image(levelOneRoom, 0, 0, this.y, this.x);
+    }
+    else if(picturesF.clicked === true){
+      //yuh
+    }
+  }
+}
+
 
 class Duotine{
   constructor( x, y, dx, dy){
     this.x = x;
     this.y = y;
-    this.size = 30; 
     this.clicked = false;
     this.whereMouseIsX = dx;
     this.whereMouseIsY = dy;
   }
   display(){
+    rect(this.x,  this.y, 108, 130);
     image(pillBottle, this.x, this.y);
   }
 
   isItCLicked(){
-    if(this.whereMouseIsX === 5){
+    if(dist(this.x, this.y, 108, 130)> this.whereMouseIsX){
       this.clicked = true;
     }                                     
   }
