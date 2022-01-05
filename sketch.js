df01239d91395fbd7a14494e73e7dfbbde4aef28
spacemossman <91297737+spacemossman@users.noreply.github.com>
2022-01-05 12:12:14 -0600

    sound dosen't work, moved back to clicking on objects
    when circus toy is clicked, switches to level 2 room for some reason. Plays the right sound, just changes the room

--- sketch.js
+++ sketch.js
@@ -4,37 +4,42 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let levelOneRoom, levelTwoRoom, pillBottle;
-let backgroundMusic;
-let picturesF, theBackground;
+let levelOneRoom, levelTwoRoom;
+let backgroundMusicLvl1, backgroungMusicLvl2;
+let circusMusic1, circusMusic2;
+let theObjects, theBackground;
 let pillHit = false;
 let toyHit = false;
+let bedHit = false;
 
 function preload(){
-  soundFormats("mp3");
   
   
-  levelOneRoom = loadImage("assets/franbow'sroom.jpg");
-  pillBottle = loadImage("assets/nobackgroundDrugs.jpg");
-  levelTwoRoom = loadImage("assets/deadroom.jpg");
-  backgroundMusic = loadSound("assets/Finding the Truth.mp3");
+  levelOneRoom = loadImage("assets/background1.png");
+  levelTwoRoom = loadImage("assets/background2.png");
+  backgroundMusicLvl1 = loadSound("assets/Dream (Ambience).mp3");
+  circusMusic1 = loadSound("assets/Children's March Theme.mp3");
+  circusMusic2 = loadSound("assets/marionettes.mp3");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  picturesF = new Objects( 1450, 650, mouseX, mouseY);
+  theObjects = new Objects( 1450, 650, mouseX, mouseY);
   theBackground = new Background();
-  backgroundMusic.play();
+  //backgroundMusic.play();
 }
 
 function draw() {
   theBackground.display();
-  picturesF.display();
+  theObjects.display();
 
 
-  pillHit = collidePointRect(mouseX, mouseY, picturesF.x, picturesF.y, 108, 130);
+  pillHit = collidePointRect(mouseX, mouseY, 1440, 630, 120, 140);
   toyHit = collidePointRect(mouseX, mouseY, 440, 540, 75, 150);
+  bedHit = collidePointRect(mouseX, mouseY, 900, 300, 245, 245);
 }
+
+
 class Background{
   constructor(){
     this.x = windowHeight;
@@ -42,11 +47,13 @@ class Background{
   }
 
   display(){
-    if(picturesF.clicked === false){
+    if(theObjects.clicked === false){
       image(levelOneRoom, 0, 0, this.y, this.x);
+      // backgroundMusicLvl1.play();
     }
-    else if(picturesF.clicked === true){
+    else if(theObjects.clicked === true){
       image(levelTwoRoom, 0, 0, this.y, this.x);
+      // backgroundMusicLvl1.stop(); 
     }
   }
 }
@@ -60,20 +67,33 @@ class Objects{
     this.whereMouseIsX = dx;
     this.whereMouseIsY = dy;
   }
+
   display(){
-    rect(440, 540, 75, 150);
-    // rect()
-    image(pillBottle, this.x, this.y);
+    rect(1440, 630, 120, 140); // pill bottle
+    rect(900, 300, 245, 245); // bed
+    rect(940, 150, 100, 100); // cross
+    rect(1150, 260, 100, 100);//papers
+    rect(100, 100, 100, 100);//clipboard
   }
 
   isItCLicked(){
     if(pillHit === true){
       this.clicked = !this.clicked;
     }
+    else if(pillHit === false){
+      this.clicked = !this.clicked;
+    }
     
     if(toyHit === true){
-      
+      if(this.clicked === false){
+        circusMusic1.play();
+      }
+      else if (this.clicked === true){
+        circusMusic2.play();
+      }
     }
+
+
   }
 
 }
@@ -81,7 +101,5 @@ class Objects{
 
 
 function mousePressed(){
-  // let cellX  = Math.floor(mouseX/cellWidth);
-  // let cellY = Math.floor(mouseY/cellHeight);
-  picturesF.isItCLicked();
+  theObjects.isItCLicked();
 }
