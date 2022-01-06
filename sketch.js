cc31d1ebfa3b7c706cc1cd385641f1df0f2a7a9c
spacemossman <91297737+spacemossman@users.noreply.github.com>
2022-01-06 12:11:21 -0600

    collide point rects solved and fixed the problem from yesterday
    next problem arises with text

--- sketch.js
+++ sketch.js
@@ -4,13 +4,28 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
+//pictures and sound
 let levelOneRoom, levelTwoRoom;
 let backgroundMusicLvl1, backgroungMusicLvl2;
 let circusMusic1, circusMusic2;
 let theObjects, theBackground;
+
+//state varible between spooky room and noraml room
+let lvl1OrLvl2; 
+
+//availble objects to click
 let pillHit = false;
 let toyHit = false;
 let bedHit = false;
+let crossHit = false;
+let papersHit  = false;
+let clipboardHit = false;
+let clownHit = false;
+let cabinetHit = false;
+let windowHit = false;
+let drapesHit = false;
+let purseHit = false;
 
 function preload(){
   
@@ -31,12 +46,21 @@ function setup() {
 
 function draw() {
   theBackground.display();
-  theObjects.display();
 
 
+  // where every item is, and if it is clikced or not
   pillHit = collidePointRect(mouseX, mouseY, 1440, 630, 120, 140);
   toyHit = collidePointRect(mouseX, mouseY, 440, 540, 75, 150);
   bedHit = collidePointRect(mouseX, mouseY, 900, 300, 245, 245);
+  crossHit  = collidePointRect(mouseX, mouseY, 940, 150, 100, 100);
+  papersHit = collidePointRect(mouseX, mouseY, 1150, 260, 100, 100);
+  clipboardHit = collidePointRect(mouseX, mouseY, 750, 240, 50, 100);
+  clownHit = collidePointRect(mouseX, mouseY, 580, 160, 110, 130);
+  cabinetHit = collidePointRect(mouseX, mouseY, 580, 340, 130, 130);
+  windowHit = collidePointRect(mouseX, mouseY, 300, 140, 240, 180);
+  drapesHit = collidePointRect(mouseX, mouseY, 270, 400, 270, 70 );
+  purseHit = collidePointRect(mouseX, mouseY, 20, 650, 160, 120);
+
 }
 
 
@@ -50,10 +74,12 @@ class Background{
     if(theObjects.clicked === false){
       image(levelOneRoom, 0, 0, this.y, this.x);
       // backgroundMusicLvl1.play();
+      lvl1OrLvl2 = 1;
     }
     else if(theObjects.clicked === true){
       image(levelTwoRoom, 0, 0, this.y, this.x);
       // backgroundMusicLvl1.stop(); 
+      lvl1OrLvl2 = 2; 
     }
   }
 }
@@ -68,34 +94,27 @@ class Objects{
     this.whereMouseIsY = dy;
   }
 
-  display(){
-    rect(1440, 630, 120, 140); // pill bottle
-    rect(900, 300, 245, 245); // bed
-    rect(940, 150, 100, 100); // cross
-    rect(1150, 260, 100, 100);//papers
-    rect(100, 100, 100, 100);//clipboard
-  }
-
   isItCLicked(){
     if(pillHit === true){
       this.clicked = !this.clicked;
     }
-    else if(pillHit === false){
-      this.clicked = !this.clicked;
-    }
     
-    if(toyHit === true){
-      if(this.clicked === false){
+    if(lvl1OrLvl2 === 1){
+      if(toyHit === true){
         circusMusic1.play();
       }
-      else if (this.clicked === true){
+    }
+    else if(lvl1OrLvl2 === 2){
+      if(toyHit === true){
         circusMusic2.play();
       }
     }
-
-
   }
 
+
+  descriptions(){
+    
+  }
 }
 
 
